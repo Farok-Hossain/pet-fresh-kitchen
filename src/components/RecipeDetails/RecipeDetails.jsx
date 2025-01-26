97b0afca318f9d7938dbf530cfaf4ffaf47cb386
Farok-Hossain <farokafs7@gmail.com>
2025-01-26 18:07:14 +0600

    recipe details page mobile reponsive

--- src/components/RecipeDetails/RecipeDetails.jsx
+++ src/components/RecipeDetails/RecipeDetails.jsx
@@ -5,15 +5,15 @@ import { CommonButton } from "@/components/common/CommonButton";
 const RecipeDetails = () => {
   return (
     <CommonWrapper>
-      <div className="flex gap-[60px] mt-[70px]">
-        <div className="min-w-[554px] h-[570px] border-[1px] flex justify-center py-[78px] px-[57px] rounded-[10px]">
-          <img src={recipeCardImg} alt="" className="min-w-[441px]" />
+      <div className="flex xl:flex-row flex-col xl:gap-[60px] gap-[30px] xl:mt-[70px]">
+        <div className="xl:min-w-[554px] xl:h-[570px] border-[1px] flex justify-center xl:py-[78px] py-5 px-5 xl:px-[57px] rounded-[10px]">
+          <img src={recipeCardImg} alt="" className="xl:min-w-[441px]" />
         </div>
         <div>
-          <h3 className="text-textGray text-5xl font-bold leading-[70px] uppercase">
+          <h3 className="text-textGray xl:text-5xl text-2xl text-center xl:text-start xl:font-bold font-medium pb-5 xl:leading-[70px] uppercase">
             Beef Mash
           </h3>
-          <p className="text-textGraySm text-xl font-normal leading-8">
+          <p className="text-textGraySm xl:text-xl font-normal text-center xl:text-start mb-5 xl:leading-8">
             Why Nutrition Matters?: Numerous studies have shown that proper
             nutrition is the foundation of a healthy, happy, and long life for
             your dog. A balanced diet supports their overall well-being, from
@@ -25,74 +25,76 @@ const RecipeDetails = () => {
             supplements to ensure your dog gets all the nutrients they need,
             including:
           </p>
-          <li className="text-textGraySm text-xl font-normal">
+          <li className="text-textGraySm xl:text-xl text-center xl:text-start font-normal">
             {" "}
             Taurine: Supports heart and eye health.
           </li>
-          <li className="text-textGraySm text-xl font-normal">
+          <li className="text-textGraySm xl:text-xl  text-center xl:text-start font-normal">
             {" "}
             Salmon Oil: Provides omega-3 fatty acids for a healthy coat and
             skin.
           </li>
-          <li className="text-textGraySm text-xl font-normal">
+          <li className="text-textGraySm xl:text-xl text-center xl:text-start font-normal">
             {" "}
             Tricalcium Phosphate: Strengthens bones and teeth.
           </li>
-          <li className="text-textGraySm text-xl font-normal">
+          <li className="text-textGraySm xl:text-xl text-center xl:text-start font-normal">
             {" "}
             Zinc, Iron, Copper, and Manganese: Vital minerals for overall
             well-being.
           </li>
-          <li className="text-textGraySm text-xl font-normal mb-8">
+          <li className="text-textGraySm xl:text-xl text-center xl:text-start font-normal xl:mb-8 mb-5">
             {" "}
             Vitamin B, D, and E Complexes: Boost immunity, energy, and
             metabolism.
           </li>
+          <div className="flex justify-center xl:justify-start">
           <CommonButton
             className="rounded text-white"
             text="Explore our Products"
           ></CommonButton>
+          </div>
         </div>
       </div>
 
-      <div className="mt-[210px]">
-        <h3 className="text-textGray text-[32px] font-semibold leading-[44px] mb-5 ">
+      <div className="xl:mt-[210px] mt-8">
+        <h3 className="text-textGray xl:text-[32px]  text-xl xl:font-semibold font-medium text-center xl:text-start xl:leading-[44px] xl:mb-5 mb-2 ">
           Chicken Chow
         </h3>
         <div className="border-b-[2px] border-[#383838] leading-8 mb-5"></div>
 
-        <p className="text-textGraySm font-normal text-xl leading-8 mb-8">
+        <p className="text-textGraySm font-normal xl:text-xl xl:leading-8 xl:mb-8 mb-5 text-center xl:text-start ">
           Ingredients: Chicken, Butternut Squash, Oats, Lentils, Spinach,
           Blueberries, Kale, Tricalcium Phosphate, Salt, Salmon Oil, Taurine,
           Zinc Gluconate, Vitamin E Supplement, Ferrous Sulfate, Copper Amino
           Acid Chelate, Manganese Amino Acid Chelate, Vitamin D3, Vitamin B2,
           Vitamin B1, Vitamin B6, Potassium Iodide.
         </p>
-        <h3 className="text-textGray text-[32px] font-semibold leading-[44px] mb-5">
+        <h3 className="text-textGray xl:text-[32px] text-xl xl:font-semibold font-medium text-center xl:text-start xl:leading-[44px] xl:mb-5 mb-2">
           Why It’s Greate
         </h3>
         <div className="border-b-[2px] border-[#383838] leading-8 mb-5"></div>
 
-        <li className="text-textGraySm text-xl font-normal mb-8">
+        <li className="text-textGraySm xl:text-xl font-normal xl:mb-8 mb-2 text-center xl:text-start">
           {" "}
           High-quality protein from chicken supports muscle health.
         </li>
-        <li className="text-textGraySm text-xl font-normal mb-8">
+        <li className="text-textGraySm xl:text-xl font-normal xl:mb-8 mb-2 text-center xl:text-start">
           {" "}
            Butternut squash and lentils provide fiber for healthy digestion
         </li>
-        <li className="text-textGraySm text-xl font-normal mb-8">
+        <li className="text-textGraySm xl:text-xl font-normal xl:mb-8 mb-2 text-center xl:text-start">
           {" "}
           Spinach, kale, and blueberries offer essential vitamins and antioxidants.
         </li>
-        <li className="text-textGraySm text-xl font-normal mb-8">
+        <li className="text-textGraySm xl:text-xl font-normal xl:mb-8 mb-5 text-center xl:text-start">
           {" "}
           Salmon oil and added vitamins promote a shiny coat and robust immune system
         </li>
-        <h3 className="text-textGray text-[32px] font-semibold leading-[44px] mb-5">Nutrition Information (Placeholder Values):</h3>
+        <h3 className="text-textGray text-center xl:text-start xl:text-[32px] text-xl xl:font-semibold font-medium xl:leading-[44px] xl:mb-5 mb-2">Nutrition Information (Placeholder Values):</h3>
         <div className="border-b-[2px] border-[#383838] leading-8 mb-5"></div>
 
-        <li className="text-textGraySm text-xl font-normal mb-8">
+        <li className="text-textGraySm xl:text-xl text-center xl:text-start font-normal xl:mb-8">
           {" "}
           Calories: 350 kcal/cup
         </li>
