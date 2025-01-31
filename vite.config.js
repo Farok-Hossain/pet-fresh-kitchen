18439eab427b4d87e83235cd74df4deae3989efe
Farok-Hossain <farokafs7@gmail.com>
2025-01-31 17:23:30 +0600

    demo commit v1

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: "/", // Ensures absolute paths
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    assetsDir: "assets", // Ensures assets go into "assets" folder
-    manifest: true, // Generates a manifest.json for debugging
-  },
-});
+})
