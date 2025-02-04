9e93fa11face687eb015eaa376fdc5da93dde90b
Farok-Hossain <farokafs7@gmail.com>
2025-02-04 14:56:07 +0600

    laptop and tablet responsive

--- src/components/common/RecipeDetails.jsx
+++ src/components/common/RecipeDetails.jsx
@@ -12,36 +12,48 @@ const RecipeDetails = ({
   return (
     <div className="container">
       {/* chicken chow  */}
-      <div className="flex gap-[60px]">
+      <div className="flex  lg:flex-row flex-col-reverse gap-[60px]">
         <div>
-          <h3 className="text-[32px] text-textGray font-semibold leading-[44px] mb-5">
+          <h3 className="xl:text-[32px] xlg:text-2xl text-xl text-textGray font-semibold xl:leading-[44px] xlg:leading-9 lg:leading-7 xlg:mb-5 lg:mb-4 mb-3">
             Chicken & Rice Delight
           </h3>
-          <h4 className="text-xl leading-8 mb-8">{title}</h4>
-          <h3 className="text-[32px] text-textGray font-semibold leading-[44px] mb-5">
+          <h4 className="xl:text-xl xlg:text-[18px] lg:text-[16px] text-[14px] xlg:leading-8 xl:mb-8 xlg:mb-6lg:mb-5 mb-4">
+            {title}
+          </h4>
+          <h3 className="xl:text-[32px] xlg:text-2xl text-xl text-textGray font-semibold xl:leading-[44px] xlg:leading-9 lg:leading-7 xlg:mb-5 lg:mb-4 mb-3">
             Why It’s Greate
           </h3>
           <div>
             {greateData.map((item) => (
-              <li className="text-xl leading-8 mb-8" key={item.id}>
+              <li
+                className="xl:text-xl xlg:text-[18px] lg:text-[16px] text-[14px] xlg:leading-8 xl:mb-8 xlg:mb-6 lg:mb-5 mb-4"
+                key={item.id}
+              >
                 {item.text}
               </li>
             ))}
           </div>
-          <h3 className="text-[32px] text-textGray font-semibold leading-[44px] mb-5">
+          <h3 className="xl:text-[32px] xlg:text-2xl text-xl text-textGray font-semibold xl:leading-[44px] xlg:leading-9 lg:leading-7 xlg:mb-5 lg:mb-4 mb-3">
             Nutrition Information (Placeholder Values):
           </h3>
           <div>
             {NutritionData.map((item) => (
-              <li className="text-xl leading-8 mb-8" key={item.id}>
+              <li
+                className="xl:text-xl xlg:text-[18px] lg:text-[16px] text-[14px] xlg:leading-8 xl:mb-8 xlg:mb-6 lg:mb-5 mb-4"
+                key={item.id}
+              >
                 {item.text}
               </li>
             ))}
           </div>
         </div>
         <div>
-          <div className="xl:min-w-[460px] min-w-[300px] border-[1px] border-[#E5E5E5] rounded-[10px]">
-            <img className="xl:p-[52px] p-8" src={img} alt="" />
+          <div className="xl:min-w-[460px] xlg:min-w-[300px] lg:min-w-[250px] md:max-w-[300px] flex   mx-auto  border-[1px] border-[#E5E5E5] rounded-[10px]">
+            <img
+              className="xl:p-[52px] xlg:p-8 lg:p-5 md:p-8"
+              src={img}
+              alt=""
+            />
           </div>
           <div className="flex justify-center xl:block pt-8">
             <CommonButton
